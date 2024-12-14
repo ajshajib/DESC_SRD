bd2996386f88d9ffc5ffddc3c3f8cac1ff90db3d
Anowar Shajib <ajshajib@uchicago.edu>
2024-12-14 17:16:25 -0600

    Add debug print statements for expint_Ei calculations in omv_vareos function

--- cosmolike_lite/theory/cosmo3D.c
+++ cosmolike_lite/theory/cosmo3D.c
@@ -121,10 +121,11 @@ double omv_vareos(double a)
   double w0, alpha;
   w0 = cosmology.w0;
   alpha = cosmology.wa + 1.45;
-  // print alpha and a
-  printf("alpha = %le\n", alpha);
-  printf("a = %le\n", a);
-  printf("expint_E1(alpha/a) = %le\n", expint_E1(alpha / a));
+  for (int i = 300; i < 500; i++)
+  {
+    printf("x = %le\n", i);
+    printf("expint_Ei(x) = %le\n", expint_Ei(i));
+  }
   // return (cosmology.Omega_v * exp(-3. * ((cosmology.w0 + cosmology.wa + 1.) * log(a) + cosmology.wa * (1. - a))));
   return (cosmology.Omega_v * exp(3. * (1 + w0) * exp(alpha) * (expint_E1(alpha) - expint_E1(alpha / a))));
 }
